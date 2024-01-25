e77b1f46122d693d26df1d495d57e5b8cacf1a30
Greg Bowne <gbowne144@gmail.com>
2024-01-24 19:10:38 -0800

    Adding new programs and fixing CMakeLists.txt version

---
+++ c_programs/resistance.c
@@ -0,0 +1,45 @@
+// File:   resistance.c
+// Author: Gregory K. Bowne
+// Date:   17 MAY 2021
+// Time:   11:34:38
+// Brief:  This program calculates series and parallel resitance.
+
+#include <stdio.h>
+
+// Function to calculate total resistance in series
+float calcSeriesResistance(float resistors[], int count) {
+    float total = 0;
+    for (int i = 0; i < count; i++) {
+        total += resistors[i];
+    }
+    return total;
+}
+
+// Function to calculate total resistance in parallel
+float calcParallelResistance(float resistors[], int count) {
+    float total = 0;
+    for (int i = 0; i < count; i++) {
+        total += 1 / resistors[i];
+    }
+    return 1 / total;
+}
+
+int main() {
+    int count;
+    printf("Enter the number of resistors: ");
+    scanf("%d", &count);
+
+    float resistors[count];
+    for (int i = 0; i < count; i++) {
+        printf("Enter the resistance of resistor %d: ", i + 1);
+        scanf("%f", &resistors[i]);
+    }
+
+    float series = calcSeriesResistance(resistors, count);
+    float parallel = calcParallelResistance(resistors, count);
+
+    printf("Total resistance in series: %.2f ohms\n", series);
+    printf("Total resistance in parallel: %.2f ohms\n", parallel);
+
+    return 0;
+}
