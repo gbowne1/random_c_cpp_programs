80aa2e08182dcccea0f752aa902648f98fc5f951
Alexandre Maia Godoi <amgodoi@gmail.com>
2024-01-31 19:21:42 -0300

    Add width and height to winsize.cpp

--- cpp_programs/winsize.cpp
+++ cpp_programs/winsize.cpp
@@ -6,10 +6,14 @@ int main() {
     struct winsize size;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
     int consoleWidth = size.ws_col;
+    int consoleHeight = size.ws_row;
 
     for (int i = 0; i < consoleWidth; i++) {
         std::cout << "X"; // Replace 'X' with the character you want to use
     }
+
+    std::cout << "Width: " << consoleWidth << std::endl;
+    std::cout << "Height: " << consoleHeight << std::endl;
 
     return 0;
 }
